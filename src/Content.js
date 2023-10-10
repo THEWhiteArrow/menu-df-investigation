1e63ef4e9536ea78729ba708efa6ce5408524659
Jakub Cerovský <jaku1198@edu.zealand.dk>
2023-10-10 14:20:16 +0200

    Changes in Content Component to match desired design

--- src/Content.js
+++ src/Content.js
@@ -1,59 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
 const Content = () => {
+  const texts = ["Driving your transformation projects 1", "Driving your transformation projects 2", "Driving your transformation projects 3"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div className="Content bg-blue-600 pt-20 bg-rb text-rw">
-      <h1 className="font-bold">Click "Menu" to show the animation</h1>
-      <p className="w-1">
-        lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum
-        dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit. lorem ipsum dolor sit amet consectetur adipisicing elit. lorem
-        ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit
-        amet consectetur adipisicing elit. lorem ipsum dolor sit amet
-        consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur
-        adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing
-        elit.
-      </p>
+    <div className="Content">
+      <div className="flex justify-evenly items-center mx-4 mt-2 p-4 smalldiv h-screen bg-gray-100 rounded-3xl">
+        <div className='flex flex-col w-[40%]'>
+          <h1 className="font-bold text-[52px] w-[60%] transition ease-in-out delay-150">{texts[currentIndex]}</h1>
+          <div className="flex justify-between">
+            {/*The next 3 things should be 1 component. Will make it in full project...*/}
+            <div className='flex bg-white w-[30%] mt-5 rounded-2xl px-3 py-2'>
+              <img src="./icon.png" className="w-7 mr-1"/>
+              <p className="self-center text-xs font-medium">Tech<br/>Transformation</p>
+            </div>
+            <div className='flex bg-white w-[30%] mt-5 rounded-2xl px-3 py-2'>
+              <img src="./icon.png" className="w-7 mr-1"/>
+              <p className="self-center text-xs font-medium">Talent<br/>Augmentation</p>
+            </div>
+            <div className='flex bg-white w-[30%] mt-5 rounded-2xl px-3 py-2'>
+              <img src="./icon.png" className="w-7 mr-1"/>
+              <p className="self-center text-xs font-medium">Skills<br/>Uplifting</p>
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col w-[30%]'>
+          <img src="./road.png" />
+        </div>
+      </div>
     </div>
   );
 };
